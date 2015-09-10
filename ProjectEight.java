89e00e204ced5cd9441352b1c05e436d8e9d6a4e
AniahLiepard <aniah.garcia@techhigh.us>
2015-09-10 10:04:17 -0400

    Logical Operators Project
    https://www.thenewboston.com/videos.php?cat=31&amp;video=17976

---
+++ ProjectEight.java
@@ -0,0 +1,13 @@
+public class ProjectEight {
+	public static void main(String args[]){
+		int type = 1;
+		
+		if (type == 1){
+			System.out.println("You may choose from Bulbasaur, Chikorita, Treecko, Turtwig, Snivy and Chespin.");
+		}if (type == 2){
+			System.out.println("You may choose from Charmander, Cyndaquil, Torchic, Chimchar, Tepig and Fennekin.");
+		}if (type == 3){
+			System.out.println("You may choose from Squirtle, Totodile, Mudkip, Piplup, Oshawott and Froakie.");
+		}
+	}
+}
